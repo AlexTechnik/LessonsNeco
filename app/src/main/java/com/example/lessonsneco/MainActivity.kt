78e119439e60fc714289b54541e6d8d4948e2fcf
John <aleksandr@giftexpress.ru>
2022-12-24 13:58:34 +0300

    Add buttons and onClick

--- app/src/main/java/com/example/lessonsneco/MainActivity.kt
+++ app/src/main/java/com/example/lessonsneco/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.lessonsneco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.lessonsneco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val a = 123
+    val b = 222
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun onClickAdd(view: View) {
+        val result = a + b
+        binding.textViewResult.text = "Результат сложения равен: $result"
+    }
+
+    fun onClickSub(view: View) {
+        val result = a - b
+        binding.textViewResult.text = "Результат вычитания равен: $result"
+    }
+    fun onClickMlp(view: View) {
+        val result = a * b
+        binding.textViewResult.text = "Результат умножения равен: $result"
     }
 }
