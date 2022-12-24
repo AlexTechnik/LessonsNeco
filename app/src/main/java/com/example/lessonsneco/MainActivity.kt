35b01c2d754853546247053c5d68f120889f8d96
John <aleksandr@giftexpress.ru>
2022-12-24 22:28:00 +0300

    Add buttons and onClick

--- app/src/main/java/com/example/lessonsneco/MainActivity.kt
+++ app/src/main/java/com/example/lessonsneco/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.lessonsneco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.lessonsneco.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonResult.setOnClickListener {
+            if (binding.editTextVal.text.isNotEmpty()) {
+                val result: Int = binding.editTextVal.text.toString().toInt()
+                var textBlogResult: String = "Вы суперзвезда!"
+                Log.d("MyLog", "Ввод числа подписчиков: $result")
+                binding.textViewResult.visibility = View.VISIBLE
+
+                when(result) {
+                    in 0..1000 -> textBlogResult = "Вы начинающий блогер"
+                    in 1000..100000 -> textBlogResult = "Вы средний блогер"
+                }
+
+                binding.textViewResult.text = textBlogResult
+            }
+        }
+
     }
 }
